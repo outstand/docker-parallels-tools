78c58426a57290105f2017e1669bc79bed3d4172
Ryan Schlesinger <ryan@ryanschlesinger.com>
2016-08-24 07:58:54 -0700

    Add git labels

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,10 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
 RUN wget https://github.com/Yelp/dumb-init/releases/download/v1.1.3/dumb-init_1.1.3_amd64.deb
 RUN dpkg -i dumb-init_*.deb
 
+ARG VCS_REF
+LABEL org.label-schema.vcs-ref=$VCS_REF \
+  org.label-schema.vcs-url="https://github.com/outstand/docker-parallels-tools"
+
 COPY ./extracted/prl-tools-lin.tar.xz /
 
 COPY ./entrypoint.sh /
--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -x -e
 
+if [ -z "$1" ]; then
+  echo "Image tag required!"
+  exit 1
+fi
+
+IMAGE_TAG=$1
+
 cp '/Applications/Parallels Desktop.app/Contents/Resources/Tools/prl-tools-lin.iso' .
 dapper
+
+if [ -z "$(git status --porcelain)" ]; then
+  docker build -t outstand/parallels-tools:${IMAGE_TAG} --build-arg VCS_REF=`git rev-parse --short HEAD` .
+else
+  echo "Warning: dirty working directory, skipping VCS_REF"
+  docker build -t outstand/parallels-tools:${IMAGE_TAG} .
+fi
