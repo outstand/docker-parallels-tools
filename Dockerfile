30ba3ed755af02e1a95ab13398fb49dcc49c1f63
Ryan Schlesinger <ryan@ryanschlesinger.com>
2017-09-12 10:13:13 -0700

    Use parallels startup scripts

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ MAINTAINER Ryan Schlesinger <ryan@outstand.com>
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
     dkms \
+    bash \
   && rm -rf /var/lib/apt/lists/*
 
 ENV DUMB_INIT_VERSION 1.2.0
@@ -15,6 +16,6 @@ LABEL org.label-schema.vcs-ref=$VCS_REF \
 
 COPY ./extracted/prl-tools-lin.tar.xz /
 
-COPY ./entrypoint.sh /
-ENTRYPOINT ["/entrypoint.sh"]
+COPY ./docker-entrypoint.sh /
+ENTRYPOINT ["/docker-entrypoint.sh"]
 CMD ["start"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/dumb-init /bin/bash
+set -e
+
+stop_tools () {
+  if [ -x /etc/init.d/prltoolsd ]; then
+    echo "Stopping tools..."
+    /etc/init.d/prltoolsd stop
+    sleep 1
+  fi
+  exit
+}
+
+if [ "$1" = 'start' ]; then
+  trap stop_tools SIGHUP SIGINT SIGTERM
+
+  echo "Installing parallels tools ..."
+  cd /
+  tar -Jxf prl-tools-lin.tar.xz
+  cd prl-tools-lin
+  ./install --install-unattended --verbose
+
+  while true; do sleep 1000; done
+else
+  exec "$@"
+fi
